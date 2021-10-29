b6dd2b72897c64f8c90010750902f4052d7ac4cc
bane tepavcevic <bt@plania.no>
2021-10-29 20:37:39 +0200

    Add json server

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,36 @@
 
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
 
    const [blogs, setBlogs] = useState([
        {title: 'My new website', body: 'lorem ipsum..', author: 'bane', id:1},
-       {title: 'Washing the car', body: 'lorem ipsum..', author: 'bane', id:2},
+       {title: 'Washing the car', body: 'lorem ipsum..', author: 'luka', id:2},
        {title: 'Explore React library', body: 'lorem ipsum..', author: 'bane', id:3}
    ])
 
+   const [name, setName] =useState('mario')
+   // We can delete item with  setBlog() using id 
+   const handleDelete = (id) => {
+       const newBlog = blogs.filter(item => item.id !== id)
+       setBlogs(newBlog)
+}
     const handleClick = (e) => {
-        console.log('hello Bane')
+        console.log('hello Bane', blogs)
+        
     }
 
-   
+useEffect( () => {
+    console.log('use effect')
+},[])
     return (  
         <div className="home">
-            {blogs.map(function(blog){
-                return(
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>{blog.body}</p>
-                        <p>Written by {blog.author}</p>
-                    </div>
-                )
-            })}
+           <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
+           <BlogList blogs = {blogs.filter( (item) => item.author === 'luka')} title="One new Chapter" handleDelete={handleDelete}/>
+           <button onClick={() => setName('luigi')}>Change name</button>
+           <p>{name}</p>
         </div>
     );
 }
